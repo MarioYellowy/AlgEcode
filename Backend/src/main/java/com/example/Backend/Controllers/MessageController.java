63e1eccf888e332ae43f57e82b3ff82d2cda4ccb
MarioYellowy <mario03eche@gmail.com>
2025-02-15 16:20:22 -0600

    Update: Conection

---
+++ Backend/src/main/java/com/example/Backend/Controllers/MessageController.java
@@ -0,0 +1,15 @@
+package com.example.Backend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MessageController {
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return "¡Hola desde Spring!";
+    }
+}
